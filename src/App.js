62822bc36f6c7e8fcbb99568b849364b423497f0
Link Strifer <linkstrifer@outlook.com>
2018-05-18 22:32:34 -0500

    add move to shelf behavior

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route } from 'react-router-dom';
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 // components import
@@ -8,10 +8,42 @@ import ListComponent from './components/list.component';
 import SearchComponent from './components/search.component';
 
 class BooksApp extends React.Component {
+  state = {
+    books: [],
+  };
+
+  componentDidMount() {
+    BooksAPI.getAll()
+      .then(books => this.setState({ books }));
+  }
+
+  moveToShelf = (data) => {
+    BooksAPI.update(data.book, data.self)
+      .then(data => {
+        const shelves = Object.getOwnPropertyNames(data);
+        const booksCopy = this.state.books;
+
+        shelves.forEach(shelf => {
+          data[shelf].forEach(bookId => {
+            const currentBook = booksCopy.find(book => book.id === bookId);
+
+            currentBook.shelf = shelf;
+          });
+        });
+
+        this.setState({ books: booksCopy });
+      });
+  }
+
   render() {
     return (
       <div className="app">
-        <Route exact path="/" component={ListComponent} />
+        <Route exact path="/" render={() => (
+          <ListComponent
+            books={this.state.books}
+            move={this.moveToShelf}
+          />
+        )} />
         <Route path="/search" component={SearchComponent} />
       </div>
     );
---
+++ src/components/book.component.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const BookComponent = ({ book, move }) => (
+  <li>
+    <div className="book">
+      <div className="book-top">
+        <div
+          className="book-cover"
+          style={{
+            backgroundImage: `url('${book.imageLinks.smallThumbnail}')`,
+            height: 193,
+            width: 128,
+          }}
+        />
+        <div className="book-shelf-changer">
+          <select
+            onChange={(event) => {
+              move({
+                book,
+                self: event.target.value,
+              });
+            }}
+            defaultValue={book.shelf}
+          >
+            <option value="none" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
+        </div>
+      </div>
+      <div className="book-title">
+        {book.title}
+      </div>
+      <div className="book-authors">
+        {book.authors.join(', ')}
+      </div>
+    </div>
+  </li>
+);
+
+export default BookComponent;
